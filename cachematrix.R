0ac043e7aa8567b5857ed39e7432ae979bb6a124
logeX <lk16112@gmail.com>
2014-08-21 13:42:56 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -23,3 +23,15 @@ makeCacheMatrix <- function(x = matrix())
          setinverse = setinverse,
          getinverse = getinverse)
 }
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
